c4e7eb41f390bb8db8e4ee8b7bddd2eafc59c8ed
Sharique Anwer <shariqueanwer@tekhnologiaindia.com>
2025-03-07 10:39:43 +0530

    updated

--- backend/src/Utils/commonValidations.ts
+++ backend/src/Utils/commonValidations.ts
@@ -2,10 +2,10 @@ import {z} from "zod"
 
 export const commonValidations = {
     id: z.string().regex(/^[a-f\d]{24}$/i, "Invalid ID format. Must be a valid ObjectId"),
-    name: z.string().min(2, "Name must be at least 2 characters long"),
+    name: z.string().min(1, "Name must be at least 2 characters long"),
     email: z.string().email("Invalid email format"),
     password: z.string().min(6, "Password must be at least 6 characters long"),
-    // school_id: z.string().regex(/^[a-f\d]{24}$/i, "Invalid school ID format. Must be a valid ObjectId"),
-    // user_id: z.string().regex(/^[a-f\d]{24}$/i, "Invalid user ID format. Must be a valid ObjectId"),
-    // role_id: z.string().regex(/^[a-f\d]{24}$/i, "Invalid role ID format. Must be a valid ObjectId"),
+    number: z.number(),
+    string: z.string().min(2, "It must be at least 2 characters long"),
+    epochTime: z.number().int().nonnegative("Epoch time must be a positive integer"),
 }
--- backend/src/api/auth/authRouter.ts
+++ backend/src/api/auth/authRouter.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import authController from "./authController";
 import { handleServiceResponse } from "../../utils/response";
+import { validateRequest } from "../../middlewares/validateMiddleware";
+import { adminRequestSchema, userRequestSchema } from "./authModel";
 
 export const authRouter = (() => {
     const router = Router();
 
-    router.post('/register', async (req, res) => {
-        const response = await authController.register(req);
+    router.post('/register-admin', validateRequest(adminRequestSchema), async (req, res) => {
+        const response = await authController.registerAdmin(req);
         handleServiceResponse(response, res);
     })
 
